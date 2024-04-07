9f95ce552a7d1d2217ca66dddf2580ae2f7933ae
Abdallah-Azme <abdallahazme199794@gmail.com>
2024-04-07 23:13:39 +0200

    structure of onboarding page, and uploadthings has implemented

---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,198 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { UserSchema } from "@/lib/validations/user";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
+import { ChangeEvent, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Textarea } from "../ui/textarea";
+import { useUploadThing } from "@/lib/uploadthing";
+import { isBase64Image } from "@/lib/utils";
+
+type AccountProfileProps = {
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
+};
+export default function AccountProfile({
+  btnTitle,
+  user,
+}: AccountProfileProps) {
+  const [files, setFiles] = useState<File[]>([]);
+  const { startUpload } = useUploadThing("media");
+  const form = useForm<z.infer<typeof UserSchema>>({
+    resolver: zodResolver(UserSchema),
+    defaultValues: {
+      username: "",
+      profile_photo: user?.image || "",
+      name: user?.bio || "",
+      bio: user?.name || "",
+    },
+  });
+
+  const handleImage = (
+    e: ChangeEvent<HTMLInputElement>,
+    fieldChange: (value: string) => void
+  ) => {
+    e.preventDefault();
+
+    if (e.target.files && e.target.files?.length > 0) {
+      const fileReader = new FileReader();
+
+      const file = e.target.files[0];
+
+      setFiles(Array.from(e.target.files));
+
+      if (!file.type.includes("image")) {
+        return;
+      }
+
+      // get called coz of filreader.readasdataurl
+      fileReader.onload = async (event) => {
+        // the event here is reading the file as data url and it has target and in it result
+        // imagedataurl is way to display the image in the browser, the data here is represented as string, the image here been decoded
+        const imageDataUrl = event.target?.result?.toString() || "";
+        console.log(imageDataUrl);
+        fieldChange(imageDataUrl);
+      };
+
+      // gonna call on load when finish loading
+      fileReader.readAsDataURL(file);
+    }
+  };
+
+  async function onSubmit(values: z.infer<typeof UserSchema>) {
+    const blob = values.profile_photo;
+    const hasImageChanged = isBase64Image(blob);
+
+    if (hasImageChanged) {
+      const imgRes = await startUpload(files);
+
+      if (imgRes && imgRes[0].url) {
+        values.profile_photo = imgRes[0].url;
+      }
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col justify-start gap-10"
+      >
+        <FormField
+          control={form.control}
+          name="profile_photo"
+          render={({ field }) => (
+            <FormItem className="flex items-center gap-4">
+              <FormLabel className="account-form_image-label">
+                {field.value ? (
+                  <Image
+                    src={field.value}
+                    alt="Profile photo"
+                    width={96}
+                    height={96}
+                    priority
+                    className="rounded-full object-contain "
+                  />
+                ) : (
+                  <Image
+                    src="/assets/profile.svg"
+                    alt="Profile photo"
+                    width={24}
+                    height={24}
+                    priority
+                    className="rounded-full object-contain "
+                  />
+                )}
+              </FormLabel>
+              <FormControl className="flex-1 text-base-semibold text-gray-200 ">
+                <Input
+                  type="file"
+                  accept="image/*"
+                  placeholder="Upload a photo"
+                  className="account-form_image-input"
+                  onChange={(e) => handleImage(e, field.onChange)}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+                Name
+              </FormLabel>
+              <FormControl className=" ">
+                <Input
+                  {...field}
+                  placeholder="Enter your name"
+                  className="account-form_input no-focus"
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+                Username
+              </FormLabel>
+              <FormControl className="flex-1 text-base-semibold text-gray-200 ">
+                <Input
+                  {...field}
+                  placeholder="Enter your username"
+                  className="account-form_input no-focus"
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="bio"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+                Bio
+              </FormLabel>
+              <FormControl className="flex-1 text-base-semibold text-gray-200 ">
+                <Textarea
+                  rows={10}
+                  {...field}
+                  placeholder="Enter your bio"
+                  className="account-form_input no-focus"
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="bg-primary-500">
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+}
